a3074c95b0d4b76846f15ff8f12d3fd51ab6106f
jaketrent <trent.jake@gmail.com>
2016-09-12 06:56:46 -0600

    exclude non-income from tithing

--- client/tithing/index.js
+++ client/tithing/index.js
@@ -18,9 +18,14 @@ function mapStateToProps(state) {
 
 function Tithing(props) {
   const lastTithe = utils.findLastTithe(props.transs)
-  const incomeSinceLastTithe = props.transs
+  const transsSinceLastTithe = props.transs
     .filter(transUtils.sinceDate.bind(null, lastTithe ? lastTithe.date : null))
     .filter(amtUtils.amtGtZero)
+  const excludedSinceLastTithe = transsSinceLastTithe
+    .filter(utils.isExcludedFromTithing)
+  const excludedTotal = amtUtils.sumWhereAmt(excludedSinceLastTithe)
+  const incomeSinceLastTithe = transsSinceLastTithe
+    .filter(t => !utils.isExcludedFromTithing(t))
   const incomeTotal = amtUtils.sumWhereAmt(incomeSinceLastTithe)
   const tithingOwed = incomeTotal / 10
 
@@ -29,6 +34,9 @@ function Tithing(props) {
       <h1>Tithing</h1>
       <h2>Last Tithe</h2>
       <List transs={lastTithe ? [lastTithe] : []} />
+      <h2>Excluded from tithing</h2>
+      <List transs={excludedSinceLastTithe} />
+      <Total label="Total excluded" amt={excludedTotal} />
       <h2>Income since last tithing</h2>
       <List transs={incomeSinceLastTithe} />
       <Total label="Total income" amt={incomeTotal} />
--- client/tithing/utils.js
+++ client/tithing/utils.js
@@ -1,6 +1,17 @@
 import { findWhere } from '../common/array'
 
 export const TITHING_CAT_ABBREV = 'tt'
+// TODO: move to dynamic flag on cat
+export const NOT_INCOME_CAT_ABBREVS = [
+  'tran',
+  'ref',
+  'bal',
+  'savcar',
+  'savcoll',
+  'savhous',
+  'savmiss',
+  'savret'
+]
 
 export function findLastTithe(transs) {
   if (!Array.isArray(transs)) return null
@@ -8,4 +19,9 @@ export function findLastTithe(transs) {
   return [...transs]
     .reverse()
     .find(trans => trans && trans.cat && trans.cat.abbrev === TITHING_CAT_ABBREV)
+}
+
+export function isExcludedFromTithing(trans) {
+  return NOT_INCOME_CAT_ABBREVS.indexOf(trans.cat.abbrev) > -1
+    || (trans.desc && /refund/i.test(trans.desc))
 }
