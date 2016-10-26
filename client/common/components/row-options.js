36b2d985405578e2e99c175edf0d2376b24d27d1
jaketrent <trent.jake@gmail.com>
2016-10-26 09:12:19 -0600

    keep focus in row options

--- client/common/components/row-options.js
+++ client/common/components/row-options.js
@@ -1,3 +1,4 @@
+import FocusTrap from 'focus-trap-react'
 import React from 'react'
 import styleable from 'react-styleable'
 
@@ -7,10 +8,12 @@ const { func } = React.PropTypes
 
 function RowOptions(props) {
   return (
-    <div className={props.css.root}>
-      <button className={props.css.option} onClick={props.onClick.bind(null, 'destroy')}>Destroy</button>
-      <button className={props.css.option} onClick={props.onClick.bind(null, 'close')}>Close</button>
-    </div>
+    <FocusTrap>
+      <div className={props.css.root}>
+        <button className={props.css.option} onClick={props.onClick.bind(null, 'destroy')}>Destroy</button>
+        <button className={props.css.option} onClick={props.onClick.bind(null, 'close')}>Close</button>
+      </div>
+    </FocusTrap>
   )
 }
 RowOptions.propTypes = {
