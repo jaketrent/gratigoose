d61a55540b794783694d370d131d9dc3029dea94
jaketrent <trent.jake@gmail.com>
2017-10-31 07:14:59 -0600

    refactor: upgrade webpack 1 -> 3

--- server/static/webpack.config.babel.js
+++ server/static/webpack.config.babel.js
@@ -11,20 +11,43 @@ module.exports = {
     filename: 'main.js'
   },
   module: {
-    loaders: [
-      { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ },
+    rules: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
       {
         test: /\.css$/,
         loaders: [
-          'style',
-          'css?modules&importLoaders=1&localIdentName=[local]---[hash:base64:5]',
-          'postcss'
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[local]---[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [postcssImport, cssnext]
+            }
+          }
         ]
       },
-      { test: /\.(jpg|png|gif)$/, loader: 'file?limit=1000&name=[name].[ext]' }
+      {
+        test: /\.(jpg|png|gif)$/,
+        loaders: [
+          {
+            loader: 'file-loader',
+            options: {
+              limit: 1000,
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
     ]
   },
-  postcss: [postcssImport, cssnext],
   externals: [
     {
       xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}'
